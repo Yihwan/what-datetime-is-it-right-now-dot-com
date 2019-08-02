fae4bb395fce9266bd213523a64d2c2cf1212acb
yihwan <hi@yihwan.kim>
2019-08-01 21:38:06 -0700

    set-up project

---
+++ src/components/layout/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './reset.css';
+import './baseStyles.css';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <main>{children}</main>
+    </>
+  );
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
